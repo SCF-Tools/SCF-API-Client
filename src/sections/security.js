16679ad004dc7a3d3d74270d263226ccf85d8c42
Artem <artem@arktd.me>
2026-05-30 02:38:13 +0500

    Feature: Add breakdown of the actions + better jsdoc [x2]

--- src/sections/security.js
+++ src/sections/security.js
@@ -40,8 +40,19 @@ export default class Security {
     }
 
     /**
+     * @typedef {Object} HeatAction
+     * @property {number} id The ID of the action.
+     * @property {string} type The reason for the action.
+     * @property {number} heat The initial amount of heat applied.
+     * @property {number} timestamp The timestamp of the action.
+     * 
+     * @typedef {Object} HeatData
+     * @property {number} heat The current total heat of the user.
+     * @property {HeatAction[]} actions The list of active actions.
+     * 
      * Returns user's heat level.
      * @param {string} id Discord ID of the user.
+     * @returns {Promise<HeatData>} An object containing the user's current heat and active actions.
      */
     async getHeat(id) {
         let response = await this.#client.sendAPIRequest(this.#section, "getHeat", "GET", [
@@ -51,7 +62,9 @@ export default class Security {
                 type: Args.GET,
             },
         ]);
-
-        return response.heat;
+        return {
+            heat: response.heat,
+            actions: response.actions,
+        }
     }
 }
